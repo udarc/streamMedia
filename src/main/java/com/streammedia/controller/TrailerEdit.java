d1e53e165e23b9a5173e542de6ae672580f62b11
udarc <juwimana@madisoncollege.edu>
2020-02-22 09:24:28 -0600

    updated user Edit Profile, and added Trailer Delete, Details and Edit and added javadoc to all other controllers

--- src/main/java/com/streammedia/controller/TrailerEdit.java
+++ src/main/java/com/streammedia/controller/TrailerEdit.java
@@ -1,4 +1,77 @@
 package com.streammedia.controller;
 
-public class TrailerEdit {
+import com.streammedia.entity.*;
+import com.streammedia.perisistence.GenericDao;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+
+/**
+ * The type Trailer edit.
+ * @author Jeanne
+ */
+@Log4j2
+@WebServlet(
+        name = "trailerEdit",
+        urlPatterns = {"/trailer-edit"}
+)
+public class TrailerEdit extends HttpServlet {
+    private GenericDao genericDao;
+
+    public void init() {
+        genericDao = new GenericDao(Trailer.class);
+    }
+    /**
+     *  Handles HTTP GET requests.
+     *
+     *@param  request               Description of the Parameter
+     *@param  response              Description of the Parameter
+     *@exception ServletException  if there is a Servlet failure
+     *@exception IOException       if there is an IO failure
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setAttribute("trailer",genericDao.getById(Integer.valueOf(request.getParameter("uid"))));
+        String url ="/trailer/trailerEdit.jsp";
+        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request,response);
+
+    }
+    /**
+     *  Handles HTTP POST requests.
+     *@param  req              Description of the Parameter
+     *@param  resp             Description of the Parameter
+     *@exception ServletException  if there is a Servlet failure
+     *@exception IOException       if there is an IO failure
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Trailer trailer = new Trailer();
+        int trailerId = Integer.valueOf(req.getParameter("uid"));
+//        int trailerId = Integer.valueOf(String.valueOf(req.getParameterValues("uid")));
+
+        trailer = (Trailer)genericDao.getById(trailerId);
+        trailer.setTitle(req.getParameter("title").trim());
+        trailer.setAuthor(req.getParameter("author"));
+        trailer.setDuration(req.getParameter("duration"));
+        trailer.setCover(req.getParameter("cover"));
+        trailer.setPublicationDate(LocalDate.parse(req.getParameter("pub_date")));
+        trailer.setLink(req.getParameter("link"));
+        trailer.setVideo(req.getParameter("video"));
+        trailer.setSummary(req.getParameter("summary").trim());
+//        trailer.setUpdateAt(LocalDate.now());
+        log.debug("Updating Trailer: " + trailer);
+        genericDao.saveOrUpdate(trailer);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/trailer/trailerDetails.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
+
--- src/main/java/com/streammedia/controller/UserEditProfile.java
+++ src/main/java/com/streammedia/controller/UserEditProfile.java
@@ -10,11 +10,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * The type Edit user profile.
+ * @author Jeanne
  */
 @Log4j2
 @WebServlet(
@@ -22,54 +26,68 @@ import java.time.LocalDate;
         urlPatterns = {"/profile-edit"}
 )
 public class UserEditProfile extends HttpServlet {
-        private GenericDao genericDao;
+    private GenericDao genericDao;
 
-        public void init() {
-            genericDao = new GenericDao(User.class);
-        }
-        /**
-         *  Handles HTTP GET requests.
-         *
-         *@param  request               Description of the Parameter
-         *@param  response              Description of the Parameter
-         *@exception ServletException  if there is a Servlet failure
-         *@exception IOException       if there is an IO failure
-         */
-        @Override
-        public void doGet(HttpServletRequest request, HttpServletResponse response)
-                throws ServletException, IOException {
-            request.setAttribute("user",genericDao.getById(Integer.parseInt(request.getParameter("id"))));
+    public void init() {
+        genericDao = new GenericDao(User.class);
+    }
+
+    /**
+     * Handles HTTP GET requests.
+     *
+     * @param request  Description of the Parameter
+     * @param response Description of the Parameter
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
+     */
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+
+        int userId = Integer.valueOf(request.getParameter("id"));
+        session.setAttribute("user", genericDao.getById(userId));
+        request.setAttribute("user", genericDao.getById(userId));
 //            request.setAttribute("user",genericDao.getById(1));
-            String url ="/account/editUserProfile.jsp";
-            RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(url);
-            dispatcher.forward(request,response);
+        String url = "/account/editUserProfile.jsp";
+        RequestDispatcher dispatcher = request.getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(request, response);
 
-        }
-        /**
-         *  Handles HTTP POST requests.
-         *@param  req              Description of the Parameter
-         *@param  resp             Description of the Parameter
-         *@exception ServletException  if there is a Servlet failure
-         *@exception IOException       if there is an IO failure
-         */
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            User user = new User();
-            int userId = Integer.valueOf(req.getParameter("id"));
-            user = (User)genericDao.getById(userId);
-            user.setEmail(req.getParameter("email"));
-            user.setUsername(req.getParameter("username"));
-            user.setFirstName(req.getParameter("firstName"));
-            user.setLastName(req.getParameter("lastName"));
-//
-            user.setBirthdate(LocalDate.parse(req.getParameter("birthday")));
-            user.setGender(req.getParameter("gender"));
-            user.setPicture(req.getParameter("profilePicture"));
-            user.setBiography(req.getParameter("biography"));
-            user.setUpdateAt(LocalDate.now());
-            log.debug("Updating User: " + user);
-            genericDao.saveOrUpdate(user);
-                RequestDispatcher dispatcher = req.getRequestDispatcher("/account/userSuccess.jsp");
-                dispatcher.forward(req, resp);
-        }
+    }
+
+    /**
+     * Handles HTTP POST requests.
+     *
+     * @param req  Description of the Parameter
+     * @param resp Description of the Parameter
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = new User();
+        int userId = Integer.valueOf(req.getParameter("id"));
+//        int userId = 1;
+//        int userId = user.getUserId();
+        log.error("Value of Get Parameter"+ userId);
+//        user = (User) genericDao.getById(Integer.valueOf(req.getParameter("id")));
+
+        user.setEmail(req.getParameter("email"));
+        user.setUsername(req.getParameter("username"));
+        user.setFirstName(req.getParameter("firstName"));
+        user.setLastName(req.getParameter("lastName"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String birthdate = req.getParameter("birthday");
+        log.debug("Test for getting birth date" +  birthdate);
+        user.setBirthdate(LocalDate.parse(birthdate,formatter));
+        user.setGender(req.getParameter("gender"));
+        user.setPicture(req.getParameter("profilePicture"));
+        user.setBiography(req.getParameter("biography"));
+        user.setUpdateAt(LocalDate.now());
+        log.debug("Updating User: " + user);
+        genericDao.saveOrUpdate(user);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/account/userSuccess.jsp");
+        dispatcher.forward(req, resp);
+    }
 }
