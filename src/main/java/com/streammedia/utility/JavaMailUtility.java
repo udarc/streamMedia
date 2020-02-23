0a4bfd5c53602168028727575f0ae75a68eb0335
udarc <juwimana@madisoncollege.edu>
2020-02-22 18:41:16 -0600

    Added contact proprties

--- src/main/java/com/streammedia/utility/JavaMailUtility.java
+++ src/main/java/com/streammedia/utility/JavaMailUtility.java
@@ -4,10 +4,13 @@ import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.*;
 
 import lombok.extern.log4j.Log4j2;
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -17,12 +20,30 @@ import org.apache.logging.log4j.core.*;
  * The type Java mail utility.
  */
 @Log4j2
-public class JavaMailUtility {
+public  class JavaMailUtility implements PropertiesLoader {
+
 
 //    private final Logger logger = (Logger) LogManager.getLogger(this.getClass());
+//    private  Properties mailProperties;
+//    private static  String username;
+//    private  static  String senderPassword;
 
-    private static final String receiverEmail = "some@gmail.com";
-    private  static final String senderPassword = "Password";
+//    private  JavaMailUtility(){
+//
+//        try {
+//            mailProperties = loadProperties("/contact.properties");
+//            username =  mailProperties.getProperty("mail.password");
+//            senderPassword =  mailProperties.getProperty("mail.password");
+//            log.debug(mailProperties);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+
+
+
 
     /**
      * Send as html.
@@ -32,18 +53,18 @@ public class JavaMailUtility {
      * @param content the content
      * @throws MessagingException the messaging exception
      */
-    public static void sendAsHtml(String toEmail, String title, String content)
+    public static void sendAsHtml(String fromEmail,String password,String toEmail, String title, String content)
             throws MessagingException {
        log.info("Sending email to " + toEmail);
 
-        Session session = createSession();
+        Session session = createSession(toEmail,password);
 
         //create message using session
         MimeMessage message = new MimeMessage(session);
-        prepareEmailMessage(message, toEmail, title, content);
+        prepareEmailMessage(message,password,fromEmail, toEmail, title, content);
 
         //sending message
-        Transport.send(message);
+        Transport.send(message,toEmail,password);
         log.info("Done");
     }
 
@@ -56,9 +77,9 @@ public class JavaMailUtility {
      * @param content the content
      * @throws MessagingException the messaging exception
      */
-    private static void prepareEmailMessage(MimeMessage message, String toEmail, String title, String content) throws MessagingException {
+    private static void prepareEmailMessage(MimeMessage message,String fromEmail,String password, String toEmail, String title, String content) throws MessagingException {
         message.setText(content);
-        message.setFrom(new InternetAddress(receiverEmail));
+        message.setFrom(new InternetAddress(fromEmail));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
         message.setSubject(title);
     }
@@ -69,16 +90,16 @@ public class JavaMailUtility {
      *
      * @return the session
      */
-    private static Session createSession() {
+    private static Session createSession(String username,String senderPassword) {
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");//Outgoing server requires authentication
         properties.put("mail.smtp.starttls.enable", "true");//TLS must be activated
-        properties.put("mail.smtp.host", "smtp.1and1.com"); //Outgoing server (SMTP) - change it to your SMTP server
+        properties.put("mail.smtp.host", "smtp.gmail.com"); //Outgoing server (SMTP) - change it to your SMTP server
         properties.put("mail.smtp.port", "587");//Outgoing port
 
         Session session = Session.getInstance(properties, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(receiverEmail, senderPassword);
+                return new PasswordAuthentication(username, senderPassword);
             }
         });
         return session;
