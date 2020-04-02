cf04e125377497bb0485e7d4db27890d627d8fdb
udarc <juwimana@madisoncollege.edu>
2020-04-01 20:55:19 -0500

    Added ResT API endpoint for all Users, user by id and username and genres

--- src/main/java/com/streammedia/entity/Genre.java
+++ src/main/java/com/streammedia/entity/Genre.java
@@ -1,5 +1,6 @@
 package com.streammedia.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import org.hibernate.annotations.Proxy;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -26,7 +28,8 @@ import java.util.Set;
 @Table(name = "Genre")
 @EqualsAndHashCode
 @ToString
-public class Genre {
+@JsonIgnoreProperties({"films"})
+public class Genre implements Serializable {
     @Id
     @Column(name = "genre_id")
     @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
