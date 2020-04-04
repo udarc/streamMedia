c92716b5c02626a31670e569bec8f72efa711c23
udarc <juwimana@madisoncollege.edu>
2020-04-04 10:42:28 -0500

    Added BkCategory test data and Read all, but Id and add test

---
+++ src/main/java/com/streammedia/controller/BKCategoryAdd.java
@@ -0,0 +1,58 @@
+package com.streammedia.controller;
+
+import com.streammedia.entity.Crew;
+import com.streammedia.entity.User;
+import com.streammedia.perisistence.GenericDao;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(
+        urlPatterns = {"/crew-new"}
+)
+@Log4j2
+public class BKCategoryAdd extends HttpServlet {
+
+    private GenericDao crewDao;
+    private GenericDao userDao;
+
+    @Override
+    public void init() throws ServletException {
+        crewDao =  new GenericDao(Crew.class);
+        userDao = new GenericDao(User.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/film/crewAddEdit.jsp");
+        dispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Crew newCrew =  new Crew();
+        User user = (User) userDao.getByPropertyEqual("username", req.getRemoteUser()).get(0);
+        newCrew.setUser(user);
+        log.debug("Username: " + user.getUsername());
+        newCrew.setFirstName(req.getParameter("firstname").trim());
+        newCrew.setLastName(req.getParameter("lastname").trim());
+        newCrew.setEmail(req.getParameter("email").trim());
+        newCrew.setProfession(req.getParameter("profession").trim());
+        newCrew.setBiography(req.getParameter("biography").trim());
+        log.debug("Crew adding: " + newCrew.getUser());
+        if(!newCrew.equals(null) && req.getRemoteUser().equals(newCrew.getUser().getUsername())){
+            crewDao.insert(newCrew);
+            resp.sendRedirect("crews");
+        } else {
+            log.debug("Error " + newCrew.getBiography());
+            req.getRequestDispatcher("/film/crewAddEdit.jsp").forward(req,resp);
+        }
+
+    }
+}
---
+++ src/test/java/com/streammedia/perisistence/BkCategoryDaoTest.java
@@ -0,0 +1,100 @@
+package com.streammedia.perisistence;
+
+import com.streammedia.entity.BkCategory;
+import com.streammedia.test.utility.Database;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+public class BkCategoryDaoTest {
+    GenericDao bkCategoryDao;
+
+    /**
+     * Creating the dao.
+     */
+    @BeforeEach
+    void setUp() {
+
+        bkCategoryDao = new GenericDao(BkCategory.class);
+
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+    }
+    /**
+     * Verifies gets all BkCategorys successfully.
+     */
+    @Test
+    void getAllBkCategorysSuccess(){
+        List<BkCategory> bkCategorys = bkCategoryDao.getAll();
+        assertEquals(6,bkCategorys.size());
+    }
+    /**
+     * Verifies a bkCategory is returned correctly based on id search
+     */
+    @Test
+    void getBkCategoryByIdSuccess(){
+        BkCategory bkCategory =  (BkCategory)bkCategoryDao.getById(5);
+        assertTrue("Horror".equals(bkCategory.getTitle()));
+        assertNotNull(bkCategory);
+
+    }
+    @Test
+    void  insertBkCategorySuccess(){
+        BkCategory newBkCategory = new BkCategory();
+        newBkCategory.setTitle("Science Fiction (Sci-Fi)");
+        newBkCategory.setDescription("Though they're often thought of in the same vein as fantasy, what distinguishes science fiction stories is that they lean heavily on themes of technology and future science. You'll find apocalyptic and dystopian novels in the sci-fi genre as well");
+        int id = bkCategoryDao.insert(newBkCategory);
+        assertTrue(id > 0);
+        assertTrue(newBkCategory.getTitle().equals("Science Fiction (Sci-Fi)"));
+    }
+//
+//    /**
+//     * Verify successful update of bkCategory
+//     */
+//    @Test
+//    void updateBkCategorySuccess() {
+//        String newTitle = "Horror";
+//        BkCategory bkCategoryToUpdate = (BkCategory)bkCategoryDao.getById(2);
+//        bkCategoryToUpdate.setTitle(newTitle);
+//        bkCategoryDao.saveOrUpdate(bkCategoryToUpdate);
+//        BkCategory retrievedBkCategory = (BkCategory)bkCategoryDao.getById(2);
+//        log.debug("Retrieve" + retrievedBkCategory);
+//        log.debug("To Update" + bkCategoryToUpdate);
+//       assertNotNull(retrievedBkCategory);
+//        assertTrue(bkCategoryToUpdate.equals(retrievedBkCategory));
+//
+//    }
+//    /**
+//     * Verify successful get by property (equal match)
+//     */
+//    @Test
+//    void getByPropertyEqualSuccess() {
+//        List<BkCategory> bkCategorys = bkCategoryDao.getByPropertyEqual("title", "Comedy");
+//        assertEquals(1, bkCategorys.size());
+//        assertEquals(3, bkCategorys.get(0).getBkCategoryId());
+//    }
+//
+//    /**
+//     * Verify successful get by property (like match)
+//     */
+//    @Test
+//    void getByPropertyLikeSuccess() {
+//        List<BkCategory> bkCategorys = bkCategoryDao.getByPropertyLike("title", "c");
+//        assertEquals(2, bkCategorys.size());
+//    }
+//
+//    /**
+//     * Verify successful delete of bkCategory
+//     */
+////    @Disabled
+//    @Test
+//    void deleteSuccess() {
+//        bkCategoryDao.delete(bkCategoryDao.getById(2));
+//        assertNull(bkCategoryDao.getById(2));
+//    }
+}
