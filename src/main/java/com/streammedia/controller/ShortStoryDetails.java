6c5f5d14e4a283f381d0ec2f74a58d28199c9e6a
udarc <juwimana@madisoncollege.edu>
2020-04-24 10:23:29 -0500

    Implemented ShortStory Edit, Delete and Read and updated JSPs

--- src/main/java/com/streammedia/controller/ShortStoryDetails.java
+++ src/main/java/com/streammedia/controller/ShortStoryDetails.java
@@ -1,4 +1,45 @@
 package com.streammedia.controller;
 
-public class ShortStoryDetails {
+import com.streammedia.entity.*;
+import com.streammedia.perisistence.GenericDao;
+import lombok.extern.log4j.Log4j2;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * The type Short story details.
+* Gets a single TShort story from and sets it as an attribute to be accessible in JSP
+* @author Jeanne
+* @version 1.0
+* @since 2020-04-24
+*/
+@Log4j2
+@WebServlet(
+        name = "storyDetails",
+        urlPatterns = {"/story-details"})
+public class ShortStoryDetails extends HttpServlet {
+    /**
+     * The Generic dao.
+     */
+
+    GenericDao storyDao;
+
+    public void init() {
+        storyDao = new GenericDao(ShortStory.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ShortStory story = (ShortStory) storyDao.getById(Integer.parseInt(request.getParameter("uid")));
+        log.debug("Getting a Single Story servlet" + story);
+        request.setAttribute("story", story);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/stories/storyDetails.jsp");
+        dispatcher.forward(request, response);
+    }
 }
