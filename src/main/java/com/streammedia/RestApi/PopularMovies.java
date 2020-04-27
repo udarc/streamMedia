2a5f3cc0ab79356b18a6f2b01867f4559c09da68
udarc <juwimana@madisoncollege.edu>
2020-04-26 23:34:03 -0500

    Added Popular API POJO, Test, servlet and JSP

---
+++ src/main/java/com/streammedia/RestApi/PopularMovies.java
@@ -0,0 +1,62 @@
+package com.streammedia.RestApi;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PopularMovies{
+
+	@JsonProperty("page")
+	private int page;
+
+	@JsonProperty("total_pages")
+	private int totalPages;
+
+	@JsonProperty("results")
+	private List<ResultsItem> results;
+
+	@JsonProperty("total_results")
+	private int totalResults;
+
+	public void setPage(int page){
+		this.page = page;
+	}
+
+	public int getPage(){
+		return page;
+	}
+
+	public void setTotalPages(int totalPages){
+		this.totalPages = totalPages;
+	}
+
+	public int getTotalPages(){
+		return totalPages;
+	}
+
+	public void setResults(List<ResultsItem> results){
+		this.results = results;
+	}
+
+	public List<ResultsItem> getResults(){
+		return results;
+	}
+
+	public void setTotalResults(int totalResults){
+		this.totalResults = totalResults;
+	}
+
+	public int getTotalResults(){
+		return totalResults;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"PopularMovies{" + 
+			"page = '" + page + '\'' + 
+			",total_pages = '" + totalPages + '\'' + 
+			",results = '" + results + '\'' + 
+			",total_results = '" + totalResults + '\'' + 
+			"}";
+		}
+}
