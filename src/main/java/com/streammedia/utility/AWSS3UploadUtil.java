1b1b4e2c11ca70eec3adeae7a866d1bcfe1166d4
udarc <juwimana@madisoncollege.edu>
2020-05-02 00:11:40 -0500

    Test Aws S3

--- src/main/java/com/streammedia/utility/AWSS3UploadUtil.java
+++ src/main/java/com/streammedia/utility/AWSS3UploadUtil.java
@@ -4,7 +4,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.AmazonS3Exception;
 import lombok.extern.log4j.Log4j2;
 
 import java.util.Properties;
@@ -17,7 +16,7 @@ public class AWSS3UploadUtil  implements PropertiesLoader {
         try {
             properties = loadProperties("aws.properties");
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e);
         }
     }
     public AmazonS3 getS3Client(){
