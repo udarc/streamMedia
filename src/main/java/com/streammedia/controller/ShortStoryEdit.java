a25844e8ada4f1350c3f705393b557550385e500
udarc <juwimana@madisoncollege.edu>
2020-05-09 19:57:49 -0500

    Added Upload to S3 in Short story Edit Servlet

--- src/main/java/com/streammedia/controller/ShortStoryEdit.java
+++ src/main/java/com/streammedia/controller/ShortStoryEdit.java
@@ -1,8 +1,12 @@
 package com.streammedia.controller;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
 import com.streammedia.entity.*;
 import com.streammedia.perisistence.GenericDao;
+import com.streammedia.utility.AWSS3UploadUtil;
 import com.streammedia.utility.JavaHelperMethods;
+import com.streammedia.utility.PropertiesLoader;
 import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
@@ -16,14 +20,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.*;
+import java.util.Properties;
 
 
 /**
  * The type Short story edit.
  * Responsible for getting form data to update User's recor
+ *
  * @author Jeanne
  * @version 1.0
- *  @since 2020-02-22
+ * @since 2020-02-22
  */
 
 @Log4j2
@@ -35,7 +41,7 @@ import java.time.*;
         maxFileSize = 1024 * 1024 * 10 * 10 * 10,      // 1GB
         maxRequestSize = 1024 * 1024 * 5 * 5 * 5   // 100MB
 )
-public class ShortStoryEdit extends HttpServlet {
+public class ShortStoryEdit extends HttpServlet implements PropertiesLoader {
     private GenericDao storyDao;
     private GenericDao userDao;
     private String projectTargetPathCover;
@@ -55,7 +61,7 @@ public class ShortStoryEdit extends HttpServlet {
                 + File.separator + className), "covers").replace("//", "/");
         // constructs path of the directory to save uploaded file
         File file = new File(projectTargetPathCover.substring(0, 39) + "src/main/webapp/media/" + className);
-        projectAppPathCover = JavaHelperMethods.createUserImagePath(file.getAbsolutePath(),"covers").replace("//", "/");
+        projectAppPathCover = JavaHelperMethods.createUserImagePath(file.getAbsolutePath(), "covers").replace("//", "/");
         log.debug(projectTargetPathCover);
     }
 
@@ -78,7 +84,7 @@ public class ShortStoryEdit extends HttpServlet {
         } else {
             response.sendRedirect("login");
         }
-            }
+    }
 
     /**
      * Handles HTTP POST requests.
@@ -90,40 +96,68 @@ public class ShortStoryEdit extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int storyId = Integer.parseInt(req.getParameter("uid"));
-        log.debug(storyId);
-        ShortStory story = (ShortStory) storyDao.getById(storyId);
-        log.debug(story.getShortStoryId());
         if (req.isUserInRole("admin")) {
-            story.setTitle(req.getParameter("title"));
-            story.setAuthor(req.getParameter("author"));
-            story.setPublicationDate(LocalDateTime.parse(req.getParameter("pub_date")));
-            Part coverPart = req.getPart("cover");
-            log.debug(coverPart.getSubmittedFileName());
-            String pathCover = story.getCover();
-            log.debug(" Cover Path: " + pathCover);
-            if (coverPart.getSubmittedFileName().isEmpty()) {
-                story.setCover(story.getCover());
-            } else {
-                String coverPath = JavaHelperMethods.saveFileName(projectAppPathCover, coverPart);
-                Path source = Paths.get(coverPath);
-                String newName = "storyCover_" + story.getShortStoryId() + coverPath.substring(
-                        coverPath.lastIndexOf("."));
-                //https://www.geeksforgeeks.org/files-deleteifexists-method-in-java-with-examples/
-                Files.deleteIfExists(Paths.get(projectAppPathCover + File.separator + newName));
-                Files.move(source, source.resolveSibling(newName));
-                Path original = Paths.get(projectAppPathCover + File.separator + newName);
-                Path destination = Paths.get(projectTargetPathCover + File.separator + newName);
-                Files.copy(original, destination, StandardCopyOption.REPLACE_EXISTING);
-                story.setCover("media/shortstory/covers/" + newName);
-            }
+            try {
+                Properties properties = loadProperties("/aws.properties");
+                int storyId = Integer.parseInt(req.getParameter("uid"));
+                log.debug(storyId);
+                ShortStory story = (ShortStory) storyDao.getById(storyId);
+                log.debug(story.getShortStoryId());
+                story.setTitle(req.getParameter("title"));
+                story.setAuthor(req.getParameter("author"));
+                story.setPublicationDate(LocalDateTime.parse(req.getParameter("pub_date")));
+                story.setDescription(req.getParameter("description").trim());
+                Part coverPart = req.getPart("cover");
+                log.debug(coverPart.getSubmittedFileName());
+                String pathCover = story.getCover();
+                log.debug(" Cover Path: " + pathCover);
+                if (!coverPart.getSubmittedFileName().isEmpty()) {
+                    String storyCoverFileName= coverPart.getSubmittedFileName().toLowerCase();
+                    if (storyCoverFileName.endsWith(".jpg") ||storyCoverFileName.endsWith(".png") ||storyCoverFileName.endsWith("jpeg")) {
+                        String accessKeyId = properties.getProperty("aws.access.key.id");
+                        String secretAccessKey = properties.getProperty("aws.secret.access.key");
+                        String region = properties.getProperty("aws.region");
+                        String bucketName = properties.getProperty("aws.bucket.name");
+                        String subdirectory = "media/" + ShortStory.class.getSimpleName().toLowerCase();
+                        String fileObjKeyName = subdirectory + "/" + story.getTitle().toLowerCase().trim()
+                                .replace(" ","_") + "_" + story.getAuthor().trim()
+                                .replace(" ", "_") +storyCoverFileName.substring(
+                               storyCoverFileName.lastIndexOf("."));
+                        String fileToUpload = JavaHelperMethods.saveFileName(System.getProperty("java.io.tmpdir"), coverPart);
 
-            story.setDescription(req.getParameter("description").trim());
-            log.debug("Updating Story: " + story.getTitle());
-            storyDao.saveOrUpdate(story);
-            String successMessage = "Successfully Updated Short Story!";
-            req.getSession().setAttribute("storyEditSuccessMessage",successMessage);
-            resp.sendRedirect("story-details?uid=" + story.getShortStoryId());
+                        AWSS3UploadUtil awsS3 = new AWSS3UploadUtil();
+                        String fileUrl = awsS3.uploadToAWSS3(coverPart, accessKeyId, secretAccessKey, region, bucketName, fileObjKeyName, fileToUpload);
+                        story.setCover(fileUrl);
+                        Files.deleteIfExists(Paths.get(fileToUpload));
+                        log.debug("Updating Story: " + story.getTitle());
+                        storyDao.saveOrUpdate(story);
+                        String successMessage = "Successfully Updated Short Story!";
+                        req.getSession().setAttribute("storyEditSuccessMessage", successMessage);
+                        resp.sendRedirect("story-details?uid=" + story.getShortStoryId());
+                    } else {
+                        String errorMessage = " Unsupported file extension! <br/>Please only upload JPG, JPEG or PNG files";
+                       req.getSession().setAttribute("unsupportedExtension",errorMessage);
+                        req.getRequestDispatcher("/stories/storyAddEdit.jsp").forward(req,resp);
+                    }
+                }
+
+            } catch (NumberFormatException numberFormatException) {
+                log.error(numberFormatException);
+            } catch (AmazonServiceException e) {
+                // The call was transmitted successfully, but Amazon S3 couldn't process
+                // it, so it returned an error response.
+                log.error(e);
+            } catch (SdkClientException e) {
+                // Amazon S3 couldn't be contacted for a response, or the client
+                // couldn't parse the response from Amazon S3.
+                log.error(e);
+            } catch (NullPointerException npe) {
+                log.error("Object Does not Exists", npe);
+            }catch (Exception exception) {
+
+            }
+        } else {
+            req.getRequestDispatcher("/stories/storyAddEdit.jsp").forward(req,resp);
         }
     }
 }
