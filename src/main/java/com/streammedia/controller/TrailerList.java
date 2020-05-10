78422d7fd0b6121da5d02c011eec00c319d16afe
udarc <juwimana@madisoncollege.edu>
2020-05-10 11:18:19 -0500

    Added Upload to S3 in Trailer Add and Edit

--- src/main/java/com/streammedia/controller/TrailerList.java
+++ src/main/java/com/streammedia/controller/TrailerList.java
@@ -7,12 +7,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
  * The type Trailer list.
  * Gets all the trailers and makes them available in JSP
+ * Sort List Options: https://www.codebyamir.com/blog/sort-list-of-objects-by-field-java
  * @author Jeanne
  * @version 1.0
  * @since 2020-02-19
@@ -34,10 +38,16 @@ public class TrailerList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        List<List<Trailer>> listTrailer = genericDao.getAllWithPagination();
+//        List<List<Trailer>> trailerList = genericDao.getAllWithPagination();
+        List<Trailer> trailerList = genericDao.getAll();
+        trailerList.sort(Comparator.comparing(Trailer::getTrailerId).reversed());
+//        List<Trailer> sortedTrailers = trailerList.stream()
+//                .sorted(Comparator.comparing(Trailer::getTrailerId).reversed())
+//                .collect(Collectors.toList());
+//        Collections.sort(trailerList, Collections.reverseOrder());
 //        List<Trailer> listTrailer= genericDao.getAllWithPagination();
-        log.debug("Getting All Trailers servlet" + listTrailer);
-        request.setAttribute("trailers", listTrailer);
+        log.debug("Getting All Trailers servlet" + trailerList);
+        request.setAttribute("trailers", trailerList);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/trailers/trailerList.jsp");
         dispatcher.forward(request, response);
     }
